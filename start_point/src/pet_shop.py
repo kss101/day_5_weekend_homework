71c21372c64abd4bf9b793b09a250b83ecbf18a5
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-06 17:21:22 +0000

    Added add_or_remove_cash function

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -7,3 +7,9 @@ def get_pet_shop_name( pet_shop ):
 def get_total_cash( pet_shop ):
     total_cash = pet_shop["admin"]["total_cash"]
     return total_cash
+
+def add_or_remove_cash( pet_shop, cash_amount ):
+    current_total_cash = get_total_cash( pet_shop )
+    new_total_cash = current_total_cash + cash_amount
+    pet_shop["admin"]["total_cash"] = new_total_cash
+
