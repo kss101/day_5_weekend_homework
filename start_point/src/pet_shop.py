0085e3a5d3bb5607dadffb363fbb7f8e7996be3c
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-06 17:05:53 +0000

    Added get_total_cash function

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,4 +1,9 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name( pet_shop_name ):
-    return pet_shop_name["name"]
+def get_pet_shop_name( pet_shop ):
+    pet_shop_name = pet_shop["name"]
+    return pet_shop_name
+
+def get_total_cash( pet_shop ):
+    total_cash = pet_shop["admin"]["total_cash"]
+    return total_cash
