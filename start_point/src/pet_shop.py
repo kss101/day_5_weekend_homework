e59814e7b89f9ebfdf73d87ac0abb24c23211683
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-07 17:39:30 +0000

    Added customer_can_afford_pet function

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -63,3 +63,11 @@ def get_customer_pet_count( customer ):
 
 def add_pet_to_customer( customer, new_pet ):
     customer["pets"].append(new_pet)
+
+def customer_can_afford_pet( customer, new_pet ):
+    customer_cash = get_customer_cash( customer )
+    new_pet_price = new_pet["price"]
+    if customer_cash - new_pet_price >= 0:
+        return True
+    else:
+        return False
